757a57e5f84289e43284212d870964f69ff48cf2
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-21 20:10:17 +0530

    Javascript

---
+++ JavaScript/Js/String.js
@@ -0,0 +1,15 @@
+let name = "Ayushi Anand";
+
+// string indices
+for(let i=0;i<15;i++){
+    console.log(name[i]);
+}
+
+// string length
+console.log(`Length of name is ${name.length}`);
+
+// string concatenation
+let firstName = "Ayushi";
+let lastName = "Anand";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
